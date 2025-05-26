74d62e862f29c83c92369b10c7ec37fa9cd5f44e
ShivanshuPrajapati212 <152085574+ShivanshuPrajapati212@users.noreply.github.com>
2025-05-26 15:49:22 +0530

    change mongo uri

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const MONGO_PASS = process.env.MONGO_PASS;
 
-const mongoURI = `mongodb+srv://shivanshuprajapati212:${MONGO_PASS}@inotebook.fbji0.mongodb.net/?retryWrites=true&w=majority&appName=iNotebook`
+const mongoURI = `mongodb+srv://shivanshu:<db_password>@inotebook.tgnlr6j.mongodb.net/?retryWrites=true&w=majority&appName=iNotebook`
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI)
